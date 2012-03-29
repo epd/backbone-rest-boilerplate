5d1f267a97dd2ffed513669f5cb07a125960cd1f
Evan Dudla <me@evandudla.com>
2012-03-28 22:04:12 -0400

    Cleaned up index, setup Mustache view rendering

--- index.php
+++ index.php
@@ -1,89 +1,24 @@
 <?php
-
 /**
- * Step 1: Require the Slim PHP 5 Framework
- *
- * If using the default file layout, the `Slim/` directory
- * will already be on your include path. If you move the `Slim/`
- * directory elsewhere, ensure that it is added to your include path
- * or update this file path as needed.
+ * Setup Slim and Mustache components.
  */
-require 'Slim/Slim.php';
+require_once 'rest/Slim.php';
+require_once 'rest/MustacheView.php';
+$app = new Slim(array(
+    'view' => new MustacheView(),
+    'templates.path' => __DIR__ . '/app/templates',
+));
 
 /**
- * Step 2: Instantiate the Slim application
- *
- * Here we instantiate the Slim application with its default settings.
- * However, we could also pass a key-value array of settings.
- * Refer to the online documentation for available settings.
+ * Route definitions.
  */
-$app = new Slim();
-
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, and `Slim::delete`
- * is an anonymous function. If you are using PHP < 5.3, the
- * second argument should be any variable that returns `true` for
- * `is_callable()`. An example GET route for PHP < 5.3 is:
- *
- * $app = new Slim();
- * $app->get('/hello/:name', 'myFunction');
- * function myFunction($name) { echo "Hello, $name"; }
- *
- * The routes below work with PHP >= 5.3.
- */
-
-//GET route
-$app->get('/', function () {
-    $template = <<<EOT
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-
-  <title>Backbone Boilerplate</title>
-
-  <!-- Application styles -->
-  <link rel="stylesheet" href="/assets/css/index.css">
-</head>
-
-<body>
-  <!-- Main container -->
-  <div role="main" id="main"></div>
-
-  <!-- Application source -->
-  <script data-main="app/config" src="/assets/js/libs/require.js"></script>
-</body>
-</html>
-
-EOT;
-    echo $template;
-});
-
-//POST route
-$app->post('/post', function () {
-    echo 'This is a POST route';
-});
-
-//PUT route
-$app->put('/put', function () {
-    echo 'This is a PUT route';
-});
-
-//DELETE route
-$app->delete('/delete', function () {
-    echo 'This is a DELETE route';
+$app->get('/', function () use($app) {
+    $app->render('layout.html', array(
+      'app_title' => 'Backbone.js REST Boilerplate',
+    ));
 });
 
 /**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This is responsible for executing
- * the Slim application using the settings and routes defined above.
+ * Initialize the application.
  */
 $app->run();
